4ca8d08574be4b729e3727660a21c46679cde014
ahmet bilgic <ahmet_bilgic@yahoo.com>
2023-04-02 10:31:44 +0300

    Add project files.

---
+++ LinkedListExample2/LinkedList.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListExample2
+{
+    public class LinkedListClass
+    {
+        public Node Top;
+        public class Node
+        {
+            public int Data;
+            public Node Next;
+            public Node(int Data)
+            {
+                this.Data = Data;
+            }
+        }
+
+        public Node Insert(int Data, Node Head)
+        {
+            if (Head == null)
+            {
+                Head = new Node(Data);
+                return Head;
+
+            }
+            else
+            {
+                var current = Head;
+                current.Next = Insert(Data, current.Next);
+                return current;
+            }
+        }
+
+        public Node Print(Node Head)
+        {
+            if (Head != null)
+            {
+                var current = Head;
+                if (current == null)
+                {
+
+                    return Head;
+                }
+                else
+                {
+                    Console.Write(current.Data + " ");
+                    current.Next = Print(current.Next);
+
+                }
+
+
+            }
+            return Head;
+
+        }
+
+    }
+}
---
+++ LinkedListExample2/Program.cs
@@ -0,0 +1,9 @@
+﻿using LinkedListExample2;
+
+var list = new LinkedListClass();
+list.Top = list.Insert(0, list.Top);
+list.Top = list.Insert(1, list.Top);
+list.Top = list.Insert(2, list.Top);
+list.Top = list.Insert(3, list.Top);
+
+list.Print(list.Top);
